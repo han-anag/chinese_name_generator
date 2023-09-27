5f6654186a756bef358f591b5818739199bdc50e
han-anag <114708039+han-anag@users.noreply.github.com>
2023-09-26 23:28:38 -0400

    reads file and splits columns

--- list_names.py
+++ list_names.py
@@ -1,47 +1,53 @@
 import csv
-import re
+import pandas as pd
 
 """Sets up generation of surnames"""
-# Open the .csv file of family name characters and Pinyin, encode without the byte order mark (BOM)
-with open('surnames.csv', newline='', encoding='utf-8-sig') as csvfile:
-    lastnames = csv.reader(csvfile)
-    # for row in lastnames:
-    #    print(''.join(row))
-    #    pass
-    #    #print(row)
+surnames = pd.read_csv('surnames.csv', header=None)
+print(surnames.head())
+surnames[0].str.split(' ', expand=True)
+print(surnames.head())
+
+
+# # Open the .csv file of family name characters and Pinyin, encode without the byte order mark (BOM)
+# with open('surnames.csv', newline='', encoding='utf-8-sig') as csvfile:
+#     lastnames = csv.reader(csvfile)
+#     # for row in lastnames:
+#     #    print(''.join(row))
+#     #    pass
+#     #    #print(row)
     
-    # An array of strings, each element being a family name
-    list_surnames = []
-    for row in lastnames:
-        list_surnames.append(row[0])
+#     # An array of strings, each element being a family name
+#     list_surnames = []
+#     for row in lastnames:
+#         list_surnames.append(row[0])
 
-    # print(list_surnames)
+#     print(list_surnames)
 
-# Remove '\xa0' character if present
-for i in range(len(list_surnames)):
-    if "\xa0" in list_surnames[i]:
-        list_surnames[i] = list_surnames[i].replace(u'\xa0', u' ')
+# # Remove '\xa0' character if present
+# for i in range(len(list_surnames)):
+#     if "\xa0" in list_surnames[i]:
+#         list_surnames[i] = list_surnames[i].replace(u'\xa0', u' ')
 
-# print(list_surnames)
+# # print(list_surnames)
 
-# Put names into dictionary
-dict_surnames = {}
-for surname in list_surnames:
-    dict_surnames[surname[0:surname.find(' ')]] = surname[(surname.find(' ')) + 1:len(surname)]
+# # Put names into dictionary
+# dict_surnames = {}
+# for surname in list_surnames:
+#     dict_surnames[surname[0:surname.find(' ')]] = surname[(surname.find(' ')) + 1:len(surname)]
 
 #print(dict_surnames)
 
-"""Sets up generation of given names"""
-with open('top_first_names_per_decade.csv', newline='', encoding='utf-8-sig') as csvfile:
-    firstnames = csv.reader(csvfile)
-    for row in firstnames:
-       # print('/'.join(row))
-       print(row)
+# """Sets up generation of given names"""
+# with open('top_first_names_per_decade.csv', newline='', encoding='utf-8-sig') as csvfile:
+#     firstnames = csv.reader(csvfile)
+#     for row in firstnames:
+#        # print('/'.join(row))
+#        print(row)
     
-    # An array of strings, each element being a family name
-    list_firstnames = []
-    for row in firstnames:
-        list_firstnames.append(row[0])
+#     # An array of strings, each element being a family name
+#     list_firstnames = []
+#     for row in firstnames:
+#         list_firstnames.append(row[0])
 
-if __name__ == "__main__":
-    print(list_firstnames)
+# if __name__ == "__main__":
+#     print(list_firstnames)
